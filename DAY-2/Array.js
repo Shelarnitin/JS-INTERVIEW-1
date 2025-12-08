40221c11f35e5eb63bc03c32ee011aaab48cd20e
Shelarnitin <shelarnitin399@gmail.com>
2025-12-08 19:06:02 +0530

    EVEN & ODD

--- DAY-2/Array.js
+++ DAY-2/Array.js
@@ -27,3 +27,37 @@ function findMax(arr) {
 
 const sampleArray1 = [2,3,4,1,17,4,6,9]
 console.log(findMax(sampleArray1));
+
+
+
+
+/******************* countEvenOdd ********************/
+
+function countEvenOdd(nums) {
+    let even_count = 0;
+    let odd_count = 0;
+
+    // Loop through array manually
+    for (let i = 0; i < nums.length; i++) {
+        const value = nums[i];
+
+        // Even number
+        if (value % 2 === 0) {
+            even_count++;
+        } 
+        // Odd number
+        else {
+            odd_count++;
+        }
+    }
+
+    return { even_count: even_count, odd_count: odd_count };
+}
+
+
+console.log(countEvenOdd([1, 2, 3, 4, 5])); 
+// { even_count: 2, odd_count: 3 }
+
+console.log(countEvenOdd([])); 
+// { even_count: 0, odd_count: 0 }
+
