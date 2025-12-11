b06054871c2532785c7d1ed967bfbe9b06082803
Shelarnitin <shelarnitin399@gmail.com>
2025-12-11 22:37:34 +0530

    Next Prime

--- DAY-3/Array.js
+++ DAY-3/Array.js
@@ -17,3 +17,37 @@ function isPerfectSquare(n) {
 
 console.log(isPerfectSquare(16));  // true
 console.log(isPerfectSquare(7));   // false
+
+
+
+// NEXT PRIME
+
+function nextPrime(n) {
+    let num = n + 1;
+
+    while (true) {
+        if (isPrime(num)) {
+            return num;
+        }
+        num++;
+    }
+}
+
+function isPrime(x) {
+    if (x < 2) return false;
+
+    let i = 2;
+
+    // Check divisors i such that i*i <= x
+    while (i * i <= x) {
+        if (x % i === 0) {
+            return false;
+        }
+        i++;
+    }
+
+    return true;
+}
+
+console.log(nextPrime(7));   // 11
+console.log(nextPrime(13));  // 17
