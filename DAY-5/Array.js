4f36b9657183d6e4066195183bb04508a08a5400
Shelarnitin <shelarnitin399@gmail.com>
2025-12-17 13:43:02 +0530

    recursive n-to-1

--- DAY-5/Array.js
+++ DAY-5/Array.js
@@ -20,3 +20,25 @@ getSum(3) returns 3 + 3 = 6
 getSum(4) returns 4 + 6 = 10
 getSum(5) returns 5 + 10 = 15
 */
+
+
+// (2) ************* recursive n-to-1 *******
+function solution(n) {
+    if (n === 1) {
+        return [1];
+    }
+
+    const rest = solution(n - 1);
+
+    const result = new Array(rest.length + 1);
+
+    result[0] = n;
+
+    for (let i = 0; i < rest.length; i++) {
+        result[i + 1] = rest[i];
+    }
+
+    return result;
+}
+
+console.log(solution(5)); // [5, 4, 3, 2, 1]
