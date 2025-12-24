e521bfec26291c5907d051fe117073619b3c6de0
Shelarnitin <shelarnitin399@gmail.com>
2025-12-24 19:59:05 +0530

    SuffleArray

--- DAY-7/Array.js
+++ DAY-7/Array.js
@@ -20,3 +20,30 @@ function solution(arr) {
 console.log(solution([1, 2, 3, 4, 5])); // 5, 4, 3, 2, 1 
 console.log(solution([10, 20, 30]));    // 30, 20, 10
 console.log(solution([7]));             // 7
+
+
+
+/*************** ShuffleArray *********************/
+
+function shuffleArray(nums, k) {
+    let front = [];
+    let moved = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        // (i + 1) because positions are 1-indexed
+        if ((i + 1) % k === 0) {
+            moved[moved.length] = nums[i];
+        } else {
+            front[front.length] = nums[i];
+        }
+    }
+
+    // append moved elements to the end
+    for (let i = 0; i < moved.length; i++) {
+        front[front.length] = moved[i];
+    }
+
+    return front;
+}
+
+console.log(shuffleArray([1, 2, 3, 4, 5, 6, 7, 8], 3));
