fd7edb99b40f6960f823b31e7f1afb64081ffb66
Shelarnitin <shelarnitin399@gmail.com>
2025-12-28 18:02:28 +0530

    removecommenElement

--- DAY-9/Array.js
+++ DAY-9/Array.js
@@ -34,3 +34,42 @@ console.log(mergeSortedArrays(
   [1, 3, 5, 7, 9],
   [2, 2, 3, 4, 6, 6]
 ));
+
+
+
+
+/*********** Remove Commen Element ***************/
+function removeCommonElements(arr1, arr2) {
+    let i = 0, j = 0;
+    const result = [];
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] === arr2[j]) {
+            // skip common element
+            i++;
+            j++;
+        } else if (arr1[i] < arr2[j]) {
+            result.push(arr1[i]);
+            i++;
+        } else {
+            result.push(arr2[j]);
+            j++;
+        }
+    }
+
+    // add remaining elements
+    while (i < arr1.length) {
+        result.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
+
+    return result;
+}
+
+removeCommonElements([2, 5, 7, 10], [1, 5, 9]);
+// Output: [1, 2, 7, 9, 10]
