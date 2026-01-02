9ca22e8465001268d98aec722d23169e1f6fee13
Shelarnitin <shelarnitin399@gmail.com>
2026-01-02 18:25:18 +0530

    merge Sort

--- DAY-10/sorting.js
+++ DAY-10/sorting.js
@@ -50,3 +50,52 @@ function insertionSort(arr) {
 
 console.log(insertionSort([5, 3, 1, 4, 2])); // [1, 2, 3, 4, 5]
 
+
+
+/********************Merge Sort ******************/
+
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const mid = Math.floor(arr.length / 2);
+
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
+
+    return merge(left, right);
+}
+
+function merge(left, right) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < left.length && j < right.length) {
+        if (left[i] <= right[j]) {
+            result.push(left[i]);
+            i++;
+        } else {
+            result.push(right[j]);
+            j++;
+        }
+    }
+
+    // Add remaining elements
+    while (i < left.length) {
+        result.push(left[i]);
+        i++;
+    }
+
+    while (j < right.length) {
+        result.push(right[j]);
+        j++;
+    }
+
+    return result;
+}
+
+console.log(mergeSort([10, 3, 2, 8, -1, 5, 1]));
+// Output: [-1, 1, 2, 3, 5, 8, 10]
+
